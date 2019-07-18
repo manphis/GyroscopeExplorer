6c7a4a0e05c65fa20e819b9924e543df39015597
easonchen <eason_chen@quantatw.com>
2019-07-18 16:03:17 +0800

    parse IMU raw data

--- app/src/main/java/com/kircherelectronics/gyroscopeexplorer/activity/ParseDataActivity.java
+++ app/src/main/java/com/kircherelectronics/gyroscopeexplorer/activity/ParseDataActivity.java
@@ -1,6 +1,7 @@
 package com.kircherelectronics.gyroscopeexplorer.activity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -9,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.kircherelectronics.fsensor.filter.gyroscope.OrientationGyroscope;
 import com.kircherelectronics.fsensor.filter.gyroscope.fusion.complimentary.OrientationFusedComplimentary;
@@ -21,9 +24,11 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -31,6 +36,7 @@ import java.util.Locale;
 public class ParseDataActivity extends AppCompatActivity {
     private static final String TAG = ParseDataActivity.class.getSimpleName();
     private final static int WRITE_EXTERNAL_STORAGE_REQUEST = 1000;
+    private final static int FILE_CHOOSE_CODE = 7;
 
 //    private String IMU_FILE = "/sdcard/ZZZ_q8h_Area_3_1562552173878.txt";
     private String IMU_FILE = "/sdcard/imu_rawdata_3";
@@ -41,18 +47,46 @@ public class ParseDataActivity extends AppCompatActivity {
     private List<Float> timeDiff = new ArrayList<Float>();
 
     private Button parseBtn;
+    private TextView filepathView;
+    private String pathHolder = null;
+    private RadioGroup rateGroup, deviceGroup, dataGroup;
 
     private enum Mode {
         GYROSCOPE_ONLY,
         COMPLIMENTARY_FILTER,
         KALMAN_FILTER;
     }
+    private enum Rate {
+        RATE1(0.00875f), RATE2(0.035f), RATE3(0.07f);
+
+        private float value;
+        private Rate(float value) {
+            this.value = value;
+        }
+
+        public float getValue() {
+            return value;
+        }
+    }
+    private enum DeviceType {
+        INSULIN,
+        Q8H
+    }
+    private enum DataType {
+        RAW_HEX,
+        RAW_INT
+    }
     private Mode mode = Mode.GYROSCOPE_ONLY;
+    private Rate rate = Rate.RATE2;
+    private DeviceType deviceType = DeviceType.INSULIN;
+    private DataType dataType = DataType.RAW_HEX;
+
     private OrientationGyroscope orientationGyroscope;
     private OrientationFusedComplimentary orientationComplimentaryFusion;
     private OrientationFusedKalman orientationKalmanFusion;
     private float[] fusedOrientation = new float[3];
     private double[] degree = new double[3];
+    private static final float to_rps = 0.0174532925f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +95,13 @@ public class ParseDataActivity extends AppCompatActivity {
         setContentView(R.layout.parse_activity);
 
         parseBtn = (Button) findViewById(R.id.parse_btn);
+        filepathView = (TextView) findViewById(R.id.path_tv);
+        rateGroup = (RadioGroup) findViewById(R.id.rate_rg);
+        deviceGroup = (RadioGroup) findViewById(R.id.device_rg);
+        dataGroup = (RadioGroup) findViewById(R.id.data_rg);
+        rateGroup.setOnCheckedChangeListener(radioListener);
+        deviceGroup.setOnCheckedChangeListener(radioListener);
+        dataGroup.setOnCheckedChangeListener(radioListener);
     }
 
     @Override
@@ -83,22 +124,61 @@ public class ParseDataActivity extends AppCompatActivity {
         reset();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        switch (requestCode) {
+            case FILE_CHOOSE_CODE:
+                if (resultCode == RESULT_OK) {
+                    pathHolder = data.getData().getPath();
+                    Log.i(TAG, "choose file path = " + pathHolder);
+//                    Toast.makeText(MainActivity.this, PathHolder, Toast.LENGTH_LONG).show();
+                    filepathView.setText(pathHolder);
+                }
+                break;
+        }
+    }
+
     public void onClickView(View v) {
         switch (v.getId()) {
             case R.id.parse_btn:
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        //parse Q8H data
-                        IMU_FILE = "/sdcard/ZZZ_q8h_Area_3_1562552173878.txt";
-                        getTimeInverval(IMU_FILE);
-                        parseFile(IMU_FILE);
+                        reset();
+                        Log.i(TAG, "start parsing; rate = " + rate.getValue() + "; device type = " + deviceType + "; data type = " + dataType);
 
+                        if (null != pathHolder) {
+                            IMU_FILE = "/sdcard/" + pathHolder.split(":")[1];
 
-//                        parseRawData(IMU_FILE);
+                            switch (deviceType) {
+                                case Q8H:
+                                    getTimeInverval(IMU_FILE);
+                                    parseFile(IMU_FILE);
+                                    break;
+
+                                case INSULIN:
+                                    switch (dataType) {
+                                        case RAW_HEX:
+                                            parseRawHexFile(IMU_FILE);
+                                            break;
+
+                                        case RAW_INT:
+                                            parseRawData(IMU_FILE);
+                                            break;
+                                    }
+                                    break;
+                            }
+                        }
                     }
                 }).start();
 
+                break;
+
+            case R.id.file_btn:
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                startActivityForResult(intent, FILE_CHOOSE_CODE);
                 break;
         }
     }
@@ -165,8 +245,9 @@ public class ParseDataActivity extends AppCompatActivity {
     }
 
     private void parseRawData(String filename) {
-        float to_rps = 0.0174532925f;
-        float sensitivity = 0.00875f;
+        float sensitivity = rate.getValue();
+        Log.i(TAG, "parse file = " + filename + " sensitivity = " + sensitivity);
+
         FileReader fr = null;
         BufferedReader br = null;
         long base_timestamp = 0;
@@ -176,7 +257,7 @@ public class ParseDataActivity extends AppCompatActivity {
             fr = new FileReader(filename);
             br = new BufferedReader(fr);
             String line = br.readLine().replace("\n", "").replace(" ", ""); //readLine()讀取一整行
-            while (line != null || !line.equals("")){
+            while (line != null && !line.equals("")){
 //                Log.i(TAG, line);
                 String[] items = line.split(",");
                 float[] gyro = new float[3];
@@ -185,14 +266,16 @@ public class ParseDataActivity extends AppCompatActivity {
                 gyro[2] = Integer.valueOf(items[2]) * sensitivity * to_rps;
 //                Log.i(TAG, "" + gyro[0] + " " + gyro[1] + " " + gyro[2]);
 
-                long measured_ts = base_timestamp + (long)((long)imu_count*10000000L);
+                long measured_ts = base_timestamp + (long)((long)imu_count*5000000L);
 
                 getOrientation(gyro, measured_ts);
                 logText(fusedOrientation, measured_ts);
 
                 imu_count++;
 
-                line = br.readLine().replace("\n", "").replace(" ", "");
+                line = br.readLine();
+                if (null != line)
+                    line = line.replace("\n", "").replace(" ", "");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -208,12 +291,104 @@ public class ParseDataActivity extends AppCompatActivity {
 
     }
 
+    private void parseRawHexFile(String filename) {
+        Log.i(TAG, "parse raw hex file = " + filename);
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        String output_speed = "/sdcard/" + pathHolder.split(":")[1] + "_outspeed";
+        String output_degree = "/sdcard/" + pathHolder.split(":")[1] + "_outdegree";
+        FileOutputStream speedOutStream = null, degreeOutStream = null;
+        OutputStreamWriter speedWriter = null, degreeWriter = null;
+
+        long base_timestamp = 0;
+        int imu_count = 0;
+
+        float total_degree = 0f;
+
+        try {
+            speedOutStream = new FileOutputStream(new File(output_speed));
+            speedWriter = new OutputStreamWriter(speedOutStream);
+            degreeOutStream = new FileOutputStream(new File(output_degree));
+            degreeWriter = new OutputStreamWriter(degreeOutStream);
+
+            fr = new FileReader(filename);
+            br = new BufferedReader(fr);
+            String line = br.readLine().replace("\n", ""); //readLine()讀取一整行
+            while (line != null && !line.equals("")){
+                Log.i(TAG, "parseRawHexFile " + line);
+                String[] items = line.split(" ");
+                float[] gyro = new float[3];
+                String rawStr = getRawGyroInt(hexStringToByte(items[0]), hexStringToByte(items[1])) + " "+
+                        getRawGyroInt(hexStringToByte(items[2]), hexStringToByte(items[3])) + " " +
+                        getRawGyroInt(hexStringToByte(items[4]), hexStringToByte(items[5])) + "\n";
+                Log.i(TAG, "hextostring = " + rawStr);
+                speedWriter.append(rawStr);
+
+                total_degree += getRawGyroInt(hexStringToByte(items[2]), hexStringToByte(items[3])) * 0.005 * rate.getValue();
+                Log.i(TAG, "total_degree = " + total_degree);
+
+
+
+                gyro[0] = getGyro(hexStringToByte(items[0]), hexStringToByte(items[1])) * to_rps;
+                gyro[1] = getGyro(hexStringToByte(items[2]), hexStringToByte(items[3])) * to_rps;
+                gyro[2] = getGyro(hexStringToByte(items[4]), hexStringToByte(items[5])) * to_rps;
+
+                long measured_ts = base_timestamp + (long)((long)imu_count*5000000L);
+
+                getOrientation(gyro, measured_ts);
+
+                logText(fusedOrientation, measured_ts);
+                String strX = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(fusedOrientation[0]) + 360) % 360);
+                String strY = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(fusedOrientation[1]) + 360) % 360);
+                String strZ = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(fusedOrientation[2]) + 360) % 360);
+                String degreeStr = strX + " " + strY + " " + strZ + " " + measured_ts + "\n";
+                Log.i(TAG, "" + degreeStr);
+                degreeWriter.append(degreeStr);
+
+                imu_count++;
+
+                line = br.readLine();
+                if (null != line)
+                    line = line.replace("\n", "");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) { br.close(); }
+                if (speedWriter != null) { speedWriter.close(); }
+                if (degreeWriter != null) { degreeWriter.close(); }
+                if (speedOutStream != null) {
+                    speedOutStream.flush();
+                    speedOutStream.close();
+                }
+                if (degreeOutStream != null) {
+                    degreeOutStream.flush();
+                    degreeOutStream.close();
+                }
+            } catch (IOException ignored) {
+            }
+        }
+        Log.i(TAG, "imu count = " + imu_count);
+    }
+
+    public static byte hexStringToByte(String s) {
+        byte data = 0;
+        data = (byte) ((Character.digit(s.charAt(0), 16) << 4) + Character.digit(s.charAt(1), 16));
+
+        return data;
+    }
+
     private void parseFile(String filename) {
         File file = new File(filename);
         byte[] bytes = new byte[18];
         int read = 0;
-        float to_rps = 0.0174532925f;
 //        float gyro_x, gyro_y, gyro_z, acc_x, acc_y, acc_z;
+        String output_speed = "/sdcard/" + pathHolder.split(":")[1] + "_outspeed";
+        String output_degree = "/sdcard/" + pathHolder.split(":")[1] + "_outdegree";
+        FileOutputStream speedOutStream = null, degreeOutStream = null;
+        OutputStreamWriter speedWriter = null, degreeWriter = null;
 
         int last_imu_ts = 0;
         int imu_ts;
@@ -224,6 +399,11 @@ public class ParseDataActivity extends AppCompatActivity {
 
         try {
             if (file.exists()) {
+                speedOutStream = new FileOutputStream(new File(output_speed));
+                speedWriter = new OutputStreamWriter(speedOutStream);
+                degreeOutStream = new FileOutputStream(new File(output_degree));
+                degreeWriter = new OutputStreamWriter(degreeOutStream);
+
                 is = new FileInputStream(file);
                 while((read = is.read(bytes)) != -1) {
 //                    Log.i(TAG, byteArrayToHex(bytes));
@@ -236,6 +416,12 @@ public class ParseDataActivity extends AppCompatActivity {
                         timestamp_count += 1;
                         imu_count = 0;
                     } else {
+                        String rawStr = getRawGyroInt(bytes[1], bytes[0]) + " "+
+                                getRawGyroInt(bytes[3], bytes[2]) + " " +
+                                getRawGyroInt(bytes[5], bytes[4]) + "\n";
+                        Log.i(TAG, "hextostring = " + rawStr);
+                        speedWriter.append(rawStr);
+
                         float[] gyro = new float[3];
                         float[] acc = new float[3];
                         gyro[0] = getGyro(bytes[1], bytes[0]) * to_rps;
@@ -258,7 +444,13 @@ public class ParseDataActivity extends AppCompatActivity {
                         }
 
                         getOrientation(gyro, measured_ts);
-                        logText(fusedOrientation, measured_ts);
+                        String strX = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(fusedOrientation[0]) + 360) % 360);
+                        String strY = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(fusedOrientation[1]) + 360) % 360);
+                        String strZ = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(fusedOrientation[2]) + 360) % 360);
+                        String degreeStr = strX + " " + strY + " " + strZ + " " + measured_ts + "\n";
+                        Log.i(TAG, "" + degreeStr);
+                        degreeWriter.append(degreeStr);
+//                        logText(fusedOrientation, measured_ts);
                     }
 //                    Log.i(TAG, "gyro_x = " + gyro_x);
                 }
@@ -271,6 +463,16 @@ public class ParseDataActivity extends AppCompatActivity {
             try {
                 if (is != null) {
                     is.close();
+                }
+                if (speedWriter != null) { speedWriter.close(); }
+                if (degreeWriter != null) { degreeWriter.close(); }
+                if (speedOutStream != null) {
+                    speedOutStream.flush();
+                    speedOutStream.close();
+                }
+                if (degreeOutStream != null) {
+                    degreeOutStream.flush();
+                    degreeOutStream.close();
                 }
             } catch (IOException ignored) {
             }
@@ -295,6 +497,18 @@ public class ParseDataActivity extends AppCompatActivity {
         String strY = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(rotation[1]) + 360) % 360);
         String strZ = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(rotation[2]) + 360) % 360);
 
+//        String strX = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(rotation[0]) + 360));
+//        String strY = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(rotation[1]) + 360));
+//        String strZ = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(rotation[2]) + 360));
+
+//        String strX = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(rotation[0])));
+//        String strY = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(rotation[1])));
+//        String strZ = String.format(Locale.getDefault(),"%.1f", (Math.toDegrees(rotation[2])));
+
+//        String strX = String.format(Locale.getDefault(),"%.3f", rotation[0]);
+//        String strY = String.format(Locale.getDefault(),"%.3f", rotation[1]);
+//        String strZ = String.format(Locale.getDefault(),"%.3f", rotation[2]);
+
         Log.i(TAG, "updateText = " + strX + " " + strY + " " + strZ + " " + timestamp);
     }
 
@@ -311,16 +525,25 @@ public class ParseDataActivity extends AppCompatActivity {
         }
     }
 
+    private int getRawGyroInt(byte msb, byte lsb) {
+//        Log.i(TAG, "" + String.format("%02x", msb) + " " + String.format("%02x", lsb));
+        int value = (msb & 0xFF) << 8 | (lsb & 0xFF);
+        if (value > 32767)
+            value = (65536 - value) * (-1);
+
+        return value;
+    }
+
     private float getGyro(byte msb, byte lsb) {
 //        Log.i(TAG, "" + String.format("%02x", msb) + " " + String.format("%02x", lsb));
         int value = (msb & 0xFF) << 8 | (lsb & 0xFF);
         if (value > 32767)
             value = (65536 - value) * (-1);
 
-//        Log.i(TAG, "raw value 2 = " + value);
+//        Log.i(TAG, "raw value = " + value);
 
 //        float gyro = (value / 32768.0f) * 250.0f;
-        float gyro = value * 0.00875f;
+        float gyro = value * rate.getValue();
 
         return gyro;
     }
@@ -336,4 +559,38 @@ public class ParseDataActivity extends AppCompatActivity {
 
         return acc;
     }
+
+    RadioGroup.OnCheckedChangeListener radioListener = new RadioGroup.OnCheckedChangeListener() {
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch(checkedId){
+                case R.id.radioButton1:
+                    rate = Rate.RATE1;
+                    break;
+                case R.id.radioButton2:
+                    rate = Rate.RATE2;
+                    break;
+                case R.id.radioButton3:
+                    rate = Rate.RATE3;
+                    break;
+
+
+                case R.id.radioButton4:
+                    deviceType = DeviceType.INSULIN;
+                    break;
+
+                case R.id.radioButton5:
+                    deviceType = DeviceType.Q8H;
+                    break;
+
+
+                case R.id.radioButton6:
+                    dataType = DataType.RAW_HEX;
+                    break;
+
+                case R.id.radioButton7:
+                    dataType = DataType.RAW_INT;
+                    break;
+            }
+        }
+    };
 }
